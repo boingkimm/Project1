046e9364b80e6a937c77bc2176f95cc85ab8921c
Kim Bohee <boingkimm@gmail.com>
2023-07-06 17:29:43 +0900

    커밋 제곱추가함

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ def add_func(n1,n2):
 def sub_func(n1,n2):
     return n1-n2
 
+def squ_func(n1,n2):
+    return n1^n2
+
 ## 전역 변수부
 num1, num2, result = 100,200,0
 
@@ -14,3 +17,6 @@ print(num1, '+', num2,'=', result)
 
 result = sub_func(num1,num2)
 print(num1, '-', num2,'=', result)
+
+result = squ_func(num1,num2)
+print(num1, '^', num2,'=', result)
